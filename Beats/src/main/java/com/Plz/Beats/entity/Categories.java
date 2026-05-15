537fcafc1ac8175eea7125cead0ff6fab5b00648
hanwool <hanwooloh01@gmail.com>
2026-05-15 20:55:20 +0900

    카테고리 추가

--- Beats/src/main/java/com/Plz/Beats/entity/Categories.java
+++ Beats/src/main/java/com/Plz/Beats/entity/Categories.java
@@ -2,7 +2,9 @@ package com.Plz.Beats.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -19,5 +21,6 @@ public class Categories {
     private Long category_id; // 레시피
 
     @Column(nullable = false, unique = true) // 유니크 키, 공백 x
+    @NotNull(message = "카테고리를 반드시 선택해야 합니다.")
     private String category_name;
 }

c724283d5500c6f8bfb347c22be8874a2fa5f1c3
HJin <rnjsgpwls514@naver.com>
2026-05-15 19:21:36 +0900

    entity 종류 추가

---
+++ Beats/src/main/java/com/Plz/Beats/entity/Review.java
@@ -0,0 +1,16 @@
+package com.Plz.Beats.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+}

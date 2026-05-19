635453df743be98482447701a9e09a08d7fef26e
HJin <rnjsgpwls514@naver.com>
2026-05-19 20:40:30 +0900

    error 수정

--- Feats/src/routes/AppRoutes.tsx
+++ Feats/src/routes/AppRoutes.tsx
@@ -12,19 +12,9 @@ import MyPageInfo from '../pages/MyPageInfo';
 import MyPageRecipe from '../pages/MyPageRecipe';
 import MyPageLike from '../pages/MyPageLike';
 import Logout from '../pages/Logout';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Delete from '../pages/Delete';
-import RecipeRegister from '../pages/reciperegister';
-=======
 import RecipeRegister from '../pages/RecipeRegister'
 import RecipeEdit from '../pages/RecipeEdit'
-=======
-import RecipeRegister from '../pages/RecipeRegister';
-import RecipeEdit from '../pages/RecipeEdit';
->>>>>>> 4238b0d63bf75f2eb6f5de035700c753e9be1356
-
->>>>>>> cf415908a59aa57ffc29ae97182df91a7b390c1e
 
 interface AppProps {
   user: User | null;
@@ -46,12 +36,8 @@ function App({ user, handleLoginSuccess }: AppProps) {
       <Route path='/mypage/recipe' element={<MyPageRecipe />} />
       <Route path='/fruit/like' element={<MyPageLike />} />
       <Route path='/logout' element={<Logout />} />
-<<<<<<< HEAD
       <Route path='/delete' element={<Delete />} />
-=======
       <Route path='/recipeMain/register' element={<RecipeEdit />} />
-
->>>>>>> 4238b0d63bf75f2eb6f5de035700c753e9be1356
     </Routes>
   );
 }

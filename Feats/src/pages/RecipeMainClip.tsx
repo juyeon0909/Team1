f5dbe04995beae3c58376254d1a5d1f5edfb6d0b
me <oversb8@gmail.com>
2026-05-20 18:13:15 +0900

    레시피

--- Feats/src/pages/RecipeMainClip.tsx
+++ Feats/src/pages/RecipeMainClip.tsx
@@ -2,9 +2,18 @@ function App() {
     console.log('자바스크립트 코딩 영역');
 
     return (
-        <>
-            스크랩 영역
-        </>
+
+
+
+
+
+            <button
+            onClick={() => navigate('/delete')}
+            style={{ padding: '10px 14px', background: '#fff', color: '#555', border: '0.5px solid #ccc', borderRadius: '6px', fontSize: '13px', cursor: 'pointer' }}
+            >
+            스크랩 취소
+            </button>
+
     );
 };
 

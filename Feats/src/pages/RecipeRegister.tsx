e2e787605ebf9f85e352bb3ac18b3e5310b0c3e8
me <oversb8@gmail.com>
2026-05-21 18:21:06 +0900

    레시피

--- Feats/src/pages/RecipeRegister.tsx
+++ Feats/src/pages/RecipeRegister.tsx
@@ -305,7 +305,7 @@ const RecipeRegister = () => {
           >
             취소
           </button>
-          {/* 커밋 체쿠 */}
+          {/* 커밋 체쿠  */}
           <button
             type="button"
             onClick={onSave}

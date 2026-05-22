7fc8beb38ae7470cc7a4cd55c5e97f20b0b77c17
bluesky <bluesky@naver.com>
2026-05-22 17:10:29 +0900

    로그인 403 에러 수정 MemberController login 수정 SecurityConfig @Bean 아예 삭제

--- Beats/src/main/java/com/Plz/Beats/config/SecurityConfig.java
+++ Beats/src/main/java/com/Plz/Beats/config/SecurityConfig.java
@@ -1,13 +1,8 @@
 package com.Plz.Beats.config;
 
-import com.Plz.Beats.service.MemberDetailsServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -66,14 +61,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            MemberDetailsServices memberDetailsServices,
-            PasswordEncoder passwordEncoder
-    ) {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(memberDetailsServices);
-        provider.setPasswordEncoder(passwordEncoder);
-        return new ProviderManager(provider);
-    }
 }

93c79cebfbd24ebbdc0f2694ae19c51d7213905e
bluesky <bluesky@naver.com>
2026-05-22 12:29:39 +0900

    CD 메모리 수정

--- Feats/src/pages/RecipeMainClip.tsx
+++ Feats/src/pages/RecipeMainClip.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
- import '../components/RecipeMainClip.css';
+import '../components/RecipeMainClip.css';
 
 interface ScrapedRecipe {
   id: number;

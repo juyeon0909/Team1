a03530a0aa5aa1e5576850115410a303174a0c0c
bluesky <bluesky@naver.com>
2026-05-22 17:00:14 +0900

    로그인 403 에러 수정 SecurityConfig 재귀 수정. @Bean

--- Beats/src/main/java/com/Plz/Beats/config/SecurityConfig.java
+++ Beats/src/main/java/com/Plz/Beats/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.Plz.Beats.config;
 
+import com.Plz.Beats.service.MemberDetailsServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -65,8 +68,12 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration config
-    ) throws Exception {
-        return config.getAuthenticationManager();
+            MemberDetailsServices memberDetailsServices,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(memberDetailsServices);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(provider);
     }
 }

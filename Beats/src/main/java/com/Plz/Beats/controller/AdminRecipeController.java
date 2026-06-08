c2c32385d63aa4ca10f5426359cc97cc43080375
bluesky <bluesky@naver.com>
2026-06-08 17:54:22 +0900

    admin레시피 페이지 수정

--- Beats/src/main/java/com/Plz/Beats/controller/AdminRecipeController.java
+++ Beats/src/main/java/com/Plz/Beats/controller/AdminRecipeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/recipes")
+@RequestMapping("/api/admin/recipes")
 @RequiredArgsConstructor
 public class AdminRecipeController {
 
